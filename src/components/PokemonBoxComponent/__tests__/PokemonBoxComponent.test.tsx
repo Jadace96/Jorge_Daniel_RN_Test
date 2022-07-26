bb6a3c0d0b72580e17e3d6fff592667e5e8f2195
Jorge Daniel <54192654+Jadace96@users.noreply.github.com>
2022-07-25 19:28:06 -0500

    Added PokemonBoxComponent.test.tsx

---
+++ src/components/PokemonBoxComponent/__tests__/PokemonBoxComponent.test.tsx
@@ -0,0 +1,35 @@
+// vendors
+import { Image, TouchableHighlight } from 'react-native';
+import { fireEvent, render, screen } from '@testing-library/react-native';
+
+// components
+import { PokemonBox } from '..';
+
+// mocks
+import { mockPokemonsDataMapped } from '../../../__mocks__/PokemonDataMocks';
+
+const mockPokemonData = mockPokemonsDataMapped[1];
+const mockProps = {
+	...mockPokemonData,
+	showFullDetails: true,
+};
+
+describe('PokemonBox component test suit', () => {
+	it('should update main image', () => {
+		render(<PokemonBox {...mockProps} />);
+
+		const MainImageUri =
+			screen.container.findAllByType(Image)[0].props.source.uri;
+
+		expect(MainImageUri).toEqual(mockPokemonData.imgUri);
+
+		const TouchableSpriteImage =
+			screen.container.findAllByType(TouchableHighlight)[1];
+
+		fireEvent.press(TouchableSpriteImage);
+
+		const MainImageUriUpdated =
+			screen.container.findAllByType(Image)[0].props.source.uri;
+		expect(MainImageUriUpdated).toEqual(mockPokemonData.sprites[0]);
+	});
+});
